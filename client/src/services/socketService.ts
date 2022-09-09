dc08404d8003fc65a3c3dc694e4804fbce7bc7f8
Zach Tang <zachtangoj@gmail.com>
2022-09-08 21:57:56 -0500

    add env variable

--- client/src/services/socketService.ts
+++ client/src/services/socketService.ts
@@ -15,7 +15,7 @@ interface ExtendedSocket extends Socket{
 const SocketService: layout = {
     socket: null,
     connectSocket(){
-        this.socket = <ExtendedSocket>io('http://34.193.62.118:9000');
+        this.socket = <ExtendedSocket>io(process.env.REACT_APP_BACKEND_IP as string);
         // this.socket = <ExtendedSocket>io('http://localhost:9000');
     },
     onUserId(){
